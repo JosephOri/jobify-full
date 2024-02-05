56198e45cd1b6813f3f7025430796767351141f1
Ori <ori100no@gmail.com>
2024-02-05 12:26:38 +0200

    added controller and route

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-console.log('Server-side code running');
+import express from 'express';
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
+import jobRouter from './routers/jobRouter.js';
 
-import { value } from './test-named.js';
-import random from './test-default.js';
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/jobs', jobRouter);
+
+app.get('/api/v1', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'route not found' });
+});
+
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}....`);
+});
