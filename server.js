0a1c07fe891b2a05112477cd5e54a29605871ae2
Ori <ori100no@gmail.com>
2024-02-06 12:56:11 +0200

    added db model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import * as dotenv from 'dotenv';
 import jobRouter from './routers/jobRouter.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -22,6 +23,14 @@ app.use('*', (req, res) => {
 });
 
 const port = process.env.PORT || 5100;
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`);
-});
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
